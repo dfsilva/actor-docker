d6850b7ca3cbda7ebc924701ca4f8551df93deda
Diego Silva <diegosiuniube@gmail.com>
2016-11-28 11:22:19 -0200

    alteracoes

--- actor-haproxy/bootstrap.sh
+++ actor-haproxy/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export CERTS=$ACTOR_WEB_HOST,$ACTOR_API_HOST,$ACTOR_WS_HOST,$ACTOR_MT_HOST
+export CERTS=$ACTOR_WEB_HOST,$ACTOR_JOIN_HOST,$ACTOR_API_HOST,$ACTOR_WS_HOST,$ACTOR_MT_HOST
 
 /certs.sh && supervisord -n
